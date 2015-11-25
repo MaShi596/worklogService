0ce38049adf3f461dc4e4b2c97a3dadc9ac3c660
gaochao0520 <632029078@qq.com>
2015-11-25 12:29:07 +0800

    日志models和控制器

---
+++ worklogService/Controllers/RiZhiController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+using worklogService.CommonMethod;
+using worklogService.DBoperate;
+using worklogService.Models;
+using worklogService.Models.CommunicationClass;
+
+
+namespace worklogService.Controllers
+{
+    public class RiZhiController : ApiController
+    {
+        public class StaffandPerson
+        {
+           // StaffLog staff;
+            //public StaffLog Staff
+            //{
+            //    get { return staff; }
+            //    set { staff = value; }
+            //}
+            string con;
+            public string Con
+            {
+                get { return con; }
+                set { con = value; }
+            }
+
+            WkTUser per;
+            public WkTUser Per
+            {
+                get { return per; }
+                set { per = value; }
+            }
+
+        }
+
+        public class StaffAll
+        {
+            List<StaffandPerson> list;
+            public List<StaffandPerson> List
+            {
+                get { return list; }
+                set { list = value; }
+            }
+
+        }
+
+        public HttpResponseMessage GetRiZhi(int userid, int staffid)
+        {
+            string res = "";
+            BaseService baseservice = new BaseService();
+            IList nbhstaff = baseservice.ExecuteSQL("with cte as " +
+                            "( " +
+                            " select row=row_number()over(order by getdate()), * from WktuserShareUserId where SharePresonid =  " + userid.ToString() + " and WktuserShareUserId.STATE = 0 and Id > " + staffid.ToString() +
+                            ") " +
+                            " select * from cte where row between " + "1" + " and " + "10");
+
+            List<StaffandPerson> stafflist = new List<StaffandPerson>();
+
+
+            if (nbhstaff != null && nbhstaff.Count > 0)
+            {
+                foreach (StaffLog o in nbhstaff)
+                {
+                    StaffandPerson st = new StaffandPerson();
+                    
+                    st.Con = o.Content;
+                    st.Per = o.Staff;
+                    stafflist.Add(st);
+                   
+                }
+
+                StaffAll l = new StaffAll();
+                l.List = stafflist;
+
+                res = "成功";
+                string data = JsonTools.ObjectToJson(l);
+
+                var jsonStr = "{\"Mseeage\":" + "\"" + res + "\"" + "," + "\"data\":" + data + "}";
+                var result = new HttpResponseMessage(HttpStatusCode.OK)
+                {
+                    Content = new StringContent(jsonStr, Encoding.UTF8, "text/json")
+                };
+                return result;
+
+
+            }
+
+            return null;
+
+        }
+
+        public IEnumerable<long> Get()
+        {
+
+
+            BaseService baseservice = new BaseService();
+
+            IList i = baseservice.loadEntityList("select u from Comments u ");
+
+
+            long[] b = new long[2000];
+            int num = 0;
+            foreach (Comments o in i)
+            {
+                b[num] = o.Id;
+
+                num++;
+            }
+            return b;
+            //return new string[] { "value1", "value2" };
+        }
+    
+    
+    
+    }
+}
+
---
+++ worklogService/Models/Comments.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace worklogService.Models
+{
+    public class Comments:IEntity
+    {
+        private String content;
+
+        public virtual String Content
+        {
+            get { return content; }
+            set { content = value; }
+        }
+        private String commentPersonName;
+
+        public virtual String CommentPersonName
+        {
+            get { return commentPersonName; }
+            set { commentPersonName = value; }
+        }
+    }
+}
