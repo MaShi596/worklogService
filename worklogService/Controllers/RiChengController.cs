628829b3540aff803b12000b0f5002b770b7a281
gaochao0520 <632029078@qq.com>
2015-12-04 13:40:12 +0800

    日程完成

--- worklogService/Controllers/RiChengController.cs
+++ worklogService/Controllers/RiChengController.cs
@@ -17,11 +17,17 @@ namespace worklogService.Controllers
     {
         public class RiChenginfo
         {
-            string personId;
-            public string PersonId
+            string richengId;
+            public string RichengId
             {
-                get { return personId; }
-                set { personId = value; }
+                get { return richengId; }
+                set { richengId = value; }
+            }
+            string richengContent;
+            public string RichengContent
+            {
+                get { return richengContent; }
+                set { richengContent = value; }
             }
             string personName;
             public string PersonName
@@ -29,23 +35,17 @@ namespace worklogService.Controllers
                 get { return personName; }
                 set { personName = value; }
             }
-            string personDept;
-            public string PersonDept
+            string personId;
+            public string PersonId
             {
-                get { return personDept; }
-                set { personDept = value; }
+                get { return personId; }
+                set { personId = value; }
             }
-            string richengId;
-            public string RichengId
+            string personDeptName;
+            public string PersonDeptName
             {
-                get { return richengId; }
-                set { richengId = value; }
-            }
-            string contenttxt140;
-            public string Contenttxt140
-            {
-                get { return contenttxt140; }
-                set { contenttxt140 = value; }
+                get { return personDeptName; }
+                set { personDeptName = value; }
             }
             string richengTime;
             public string RichengTime
@@ -53,24 +53,14 @@ namespace worklogService.Controllers
                 get { return richengTime; }
                 set { richengTime = value; }
             }
-            string contenttxtAll;
-            public string ContenttxtAll
+            string richengSub;
+
+            public string RichengSub
             {
-                get { return contenttxtAll; }
-                set { contenttxtAll = value; }
+                get { return richengSub; }
+                set { richengSub = value; }
             }
-            string logtick;
-            public string Logtick
-            {
-                get { return logtick; }
-                set { logtick = value; }
-            }
-            string personMD5code;
-            public string PersonMD5code
-            {
-                get { return personMD5code; }
-                set { personMD5code = value; }
-            }
+            
         }
         public class RiChengAll
         {
@@ -81,29 +71,116 @@ namespace worklogService.Controllers
                 set { list = value; }
             }
         }
-        public HttpResponseMessage GetRiCheng(string userid)
+        public HttpResponseMessage GetAllRiCheng(string userid, string rctime)
         {
+            string sqlstr = null;
+            if (rctime == "0")
+            {
+                sqlstr = "with cte as " +
+                            "( " +
+                            " select row=row_number()over(order by getdate()), * from LOG_T_STAFFSCHEDULE where  ScheduleTime > " + rctime.ToString() +
+                            ") " +
+                            " select * from cte where row between " + "1" + " and " + "10";
+            }
+            else
+            {
+                sqlstr = "with cte as " +
+                            "( " +
+                            " select row=row_number()over(order by getdate()), * from LOG_T_STAFFSCHEDULE where  ScheduleTime < " + rctime.ToString() +
+                            ") " +
+                            " select * from cte where row between " + "1" + " and " + "10";
+            }
             string res = "";
-            long thisDay = DateTime.Now.Date.Ticks;
             BaseService baseservice = new BaseService();
-            IList nbhstaff = baseservice.loadEntityList("from StaffSchedule where STATE=" + (int)IEntity.stateEnum.Normal + " and Staff=" + userid + " and ScheduleTime>=" + thisDay + " order by ScheduleTime asc");
+            IList nbhstaff = baseservice.ExecuteSQL(sqlstr);
+            List<RiChenginfo> info = new List<RiChenginfo>();
             if (nbhstaff.Count > 0 && nbhstaff != null)
             {
-                List<RiChenginfo> staff = new List<RiChenginfo>();
-                foreach(StaffSchedule n in nbhstaff)
+                for (int i = 0; i < nbhstaff.Count; i++)
                 {
-                    RiChenginfo ri = new RiChenginfo();
-                    ri.RichengId = n.Id.ToString();
-                    ri.PersonId = n.Staff.Id.ToString();
-                    ri.PersonName = n.Staff.KuName;
-                    ri.PersonMD5code = n.Staff.ImgMD5Code;
-                    ri.RichengTime = new DateTime(n.ScheduleTime).ToString("yyyy年MM月dd日 HH:mm");
-                    ri.ContenttxtAll = n.Content;
-                    
-                    staff.Add(ri);
+                    object[] rc = (object[])nbhstaff[i];
+                    RiChenginfo ric = new RiChenginfo();
+
+                    ric.RichengId = rc[1].ToString();
+                    ric.PersonId = rc[6].ToString();
+                    ric.RichengContent = rc[2].ToString();
+                    ric.RichengTime = new DateTime(Convert.ToInt64(rc[3])).ToString("yyyy年MM月dd日 HH:mm");
+                    WkTUser user = new WkTUser();
+                    user = (WkTUser)baseservice.loadEntity(user, Convert.ToInt64(rc[6]));
+                    ric.PersonName = user.KuName;
+                    ric.PersonDeptName = user.Kdid.KdName.ToString();
+                    ric.RichengSub = rc[4].ToString();
+                    info.Add(ric);
                 }
+
                 RiChengAll l = new RiChengAll();
-                l.List =staff;
+                l.List = info;
+                res = "成功";
+                string data = JsonTools.ObjectToJson(l);
+                var jsonStr = "{\"Message\":" + "\"" + res + "\"" + "," + "\"data\":" + data + "}";
+                var result = new HttpResponseMessage(HttpStatusCode.OK)
+                {
+                    Content = new StringContent(jsonStr, Encoding.UTF8, "text/json")
+                };
+                return result;
+            }
+            else
+            {
+                res = "没有内容";
+                string data = "1";//JsonTools.ObjectToJson(l);
+                var jsonStr = "{\"Message\":" + "\"" + res + "\"" + "," + "\"data\":" + data + "}";
+                var result = new HttpResponseMessage(HttpStatusCode.OK)
+                {
+                    Content = new StringContent(jsonStr, Encoding.UTF8, "text/json")
+                };
+                return result;
+            }
+        }
+
+        public HttpResponseMessage GetOwnRiCheng(string userid, string rctime)
+        {
+            string sqlstr = null;
+            if (rctime == "0")
+            {
+                sqlstr = "with cte as " +
+                            "( " +
+                            " select row=row_number()over(order by getdate()), * from LOG_T_STAFFSCHEDULE where  ScheduleTime > " + rctime.ToString() + "and WkTUserId=" + userid.ToString() +
+                            ") " +
+                            " select * from cte where row between " + "1" + " and " + "10";
+            }
+            else
+            {
+                sqlstr = "with cte as " +
+                            "( " +
+                            " select row=row_number()over(order by getdate()), * from LOG_T_STAFFSCHEDULE where  ScheduleTime < " + rctime.ToString() + "and WkTUserId=" + userid.ToString() +
+                            ") " +
+                            " select * from cte where row between " + "1" + " and " + "10";
+            }
+            string res = "";
+            BaseService baseservice = new BaseService();
+            IList nbhstaff = baseservice.ExecuteSQL(sqlstr);
+            List<RiChenginfo> info = new List<RiChenginfo>();
+            if (nbhstaff.Count > 0 && nbhstaff != null)
+            {
+                for (int i = 0; i < nbhstaff.Count; i++)
+                {
+                    object[] rc = (object[])nbhstaff[i];
+                    RiChenginfo ric = new RiChenginfo();
+
+                    ric.RichengId = rc[1].ToString();
+                    ric.PersonId = rc[6].ToString();
+                    ric.RichengContent = rc[2].ToString();
+                    ric.RichengTime = new DateTime(Convert.ToInt64(rc[3])).ToString("yyyy年MM月dd日 HH:mm");
+                    WkTUser user = new WkTUser();
+                    user = (WkTUser)baseservice.loadEntity(user, Convert.ToInt64(rc[6]));
+                    ric.PersonName = user.KuName;
+                    ric.PersonDeptName = user.Kdid.KdName.ToString();
+                    ric.RichengSub = rc[4].ToString();
+                    info.Add(ric);
+                }
+
+                RiChengAll l = new RiChengAll();
+                l.List = info;
                 res = "成功";
                 string data = JsonTools.ObjectToJson(l);
                 var jsonStr = "{\"Message\":" + "\"" + res + "\"" + "," + "\"data\":" + data + "}";
